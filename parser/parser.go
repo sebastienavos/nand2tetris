30a6d05444103022fc6a86c45a1c3cf9e186392a
Sebastien Williams <s.williams01@hotmail.co.uk>
2025-06-01 22:56:58 +0100

    parser symbol can return variable for a instruction

--- parser/parser.go
+++ parser/parser.go
@@ -2,10 +2,8 @@ package parser
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -77,11 +75,7 @@ func (p *parser) CommandType() CommandType {
 // Should be called only when commandType() is A_COMMAND or L_COMMAND.
 func (p *parser) Symbol() string {
 	if p.CommandType() == A_COMMAND {
-		i, err := strconv.Atoi(p.currentCommand[1:])
-		if err != nil {
-			panic(err)
-		}
-		return fmt.Sprintf("%015b", i)
+		return p.currentCommand[1:]
 	}
 	return p.currentCommand[1 : len(p.currentCommand)-1]
 }
